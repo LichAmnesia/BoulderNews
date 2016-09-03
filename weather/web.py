e31f639edb843a1ffeca8a81dd6fc82066e34701
Lich_Amnesia <alwaysxiaop@gmail.com>
2016-09-03 16:23:54 -0600

    update web

--- weather/web.py
+++ weather/web.py
@@ -2,7 +2,7 @@
 # @Author: Lich_Amnesia
 # @Email: alwaysxiaop@gmail.com
 # @Date:   2016-08-30 10:06:19
-# @Last Modified time: 2016-09-03 16:18:35
+# @Last Modified time: 2016-09-03 16:23:34
 # @FileName: web.py
 import tornado.ioloop
 import tornado.web
@@ -19,12 +19,13 @@ def getText(filename):
     items = []
     for i in range(len(res)):
         bk = res[i]
-        print tornado.escape.xhtml_escape('\'')
+        # tornado.escape.xhtml_escape('\'')
         Text = '''
                 The weather is updated at {0}. Now the temperature is about {1}°C.
-                Today’s temperature around {2}/{3}°C. And weather will be {4}.
+                Today‘s temperature around {2}/{3}°C. And weather will be {4}.
                 Also the wind is {5}mph and the humidity is {6}%.
             '''.format(bk[7], bk[1], bk[2], bk[3], bk[4], bk[5], bk[6])
+        # tornado.escape.xhtml_escape(Text)
         items.append(Text)
         # print('[{0}] The text is:\n{1}'.format(time.strftime(
         #     "%Y-%m-%d %H:%M:%S", time.localtime()), Text))
@@ -35,7 +36,7 @@ class MainHandler(tornado.web.RequestHandler):
     def get(self):
         items = getText('weather.db')
         # items = ["Item 1", "Item 2", "Item 3"]
-        self.render("template.html", title="My title", items=items)
+        self.render("template.html", title="Boulder Weather Report", items=items)
 
 
 def make_app():
